e01561b9b95bad7af75353639c76984041ac679f
rainsen <yusen@rainsen.com>
2018-12-27 00:31:17 +0000

    home swiper

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,11 +4,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './static/styles/reset.css'
 import './static/styles/border.css'
 import './static/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
+Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 const app = new Vue({
     el: '#app',
